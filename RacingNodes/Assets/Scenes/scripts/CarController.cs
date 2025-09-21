9bbf45c3db897c9e6eecca43ca5c090b7ff3a911
PhoenixRider8 <193403575+emberwing-dash@users.noreply.github.com>
2025-09-21 09:47:12 +0530

    update race

---
+++ RacingNodes/Assets/Scenes/scripts/CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotateAngle = 15f; // angle per button press
+
+    [SerializeField] FixedJoystick fJK;
+    private Rigidbody rb;
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float xVal = fJK.Horizontal;
+        float yVal = fJK.Vertical;
+
+        Vector3 movement = new Vector3(xVal, 0, yVal);
+
+        // Move only (no auto rotation)
+        rb.velocity = movement * speed * Time.deltaTime;
+    }
+
+    // 🚗 Rotate Left
+    public void RotateLeft()
+    {
+        transform.Rotate(0, -rotateAngle, 0);
+    }
+
+    // 🚗 Rotate Right
+    public void RotateRight()
+    {
+        transform.Rotate(0, rotateAngle, 0);
+    }
+}
